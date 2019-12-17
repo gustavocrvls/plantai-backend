2bc213c6979b575f635ed2194254f8189d65dd6c
Gustavo Carvalho <gustavocrvl42@gmail.com>
2019-12-17 16:33:04 -0300

    arquivo env

--- config/server.js
+++ config/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan')
-const consign = require('consign')
 const passport = require('passport')
 const morgan = require('morgan')
+const dotenv = require('dotenv');
 // const API_PORT = process.env.PORT || 3001;
 const API_PORT = 3001
 
@@ -12,7 +12,8 @@ module.exports = function () {
     const app = express();
     app.use(cors());
     app.set('port', API_PORT)
-
+    dotenv.config();
+    
     app.use(morgan('dev'));
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
@@ -27,6 +28,7 @@ module.exports = function () {
     
     app.use(passport.initialize());
     app.use(passport.session());
-
+    
+    
     return app;
 }
